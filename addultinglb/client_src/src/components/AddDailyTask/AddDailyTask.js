4d7eb21c7020815af6415e2d36f8585f666021e2
jrtrent <jessrtrent@gmail.com>
2018-07-28 01:00:07 -0400

    add tasks

--- addultinglb/client_src/src/components/AddDailyTask/AddDailyTask.js
+++ addultinglb/client_src/src/components/AddDailyTask/AddDailyTask.js
@@ -5,6 +5,19 @@ import axios from 'axios';
 
 
 class AddDailyTask extends Component  {
+    constructor(){
+        super();
+        this.state={
+            name:"",
+            category:"",
+            priority: "",
+            estTime: "",
+            recurring: ""
+           
+        }
+    }
+
+
     AddDailyTask(newdailyTask) {
         axios.request({
             method:"post",
@@ -24,15 +37,22 @@ class AddDailyTask extends Component  {
         var instances = window.M.FormSelect.init(elems, {});
     }
 
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
     handleFormSubmit = (e) => {
         e.preventDefault();
        
         const newdailyTask = {
-            name: this.refs.name.value,
-            category:this.refs.category.value,
-            priority: this.refs.priority.value,
-            estTime: this.refs.estTime.value,
-            recurring: this.refs.recurring.value
+            name: this.state.name,
+            category:this.state.category,
+            priority: this.state.priority,
+            estTime: this.state.estTime,
+            recurring: this.state.recurring
 
         }
         this.AddDailyTask(newdailyTask);
@@ -48,33 +68,57 @@ class AddDailyTask extends Component  {
               <br />
               <form onSubmit={this.handleFormSubmit.bind(this)}>
                 <div className="input-field">
-                    <input type="text" name="name" ref="name" />
+                    <input type="text" name="name"  onChange={this.handleInputChange.bind(this)}/>
                     <label htmlFor="name">TaskName</label>
                 </div>
-                <div className="input-field">
-                    <input type="text" name="category" ref="category" />
-                    <label htmlFor="category">Category</label>
+                <div className="input-field col s12">
+            
+                    <select value={this.state.category} name="category" onChange={this.handleInputChange.bind(this)}>
+                        <option value="" disabled selected>Category</option>
+                        <option value="work">Work</option>
+                        <option value="home">Home</option>
+                        <option value="social">Social</option>
+                        <option value="family">Family</option>
+                    </select>
+                    <label></label>
                 </div>
                 <div className="input-field col s12">
                     {/* <input type="text" name="priority" ref="priority" /> */}
             
-                    <select>
-                        <option value="" disabled selected>Choose your option</option>
-                        <option value="low">Low</option>
-                        <option value="medium">medium</option>
-                        <option value="high">high</option>
+                    <select value={this.state.priority} name="priority" onChange={this.handleInputChange.bind(this)}>
+                        <option value="" disabled selected>Priority</option>
+                        <option value="3">Low</option>
+                        <option value="2">Medium</option>
+                        <option value="1">High</option>
                     </select>
-                    <label>Priority</label>
+                    <label></label>
                 </div>
-                <div className="input-field">
-                    <input type="text" name="estTime" ref="estTime" />
-                    <label htmlFor="estTime">estTime</label>
+                <div className="input-field col s12">
+            
+                    <select value={this.state.estTime} name="estTime" onChange={this.handleInputChange.bind(this)}>
+                        <option value="" disabled selected>EstimatedTime</option>
+                        <option value="15">15 min</option>
+                        <option value="30">30 min</option>
+                        <option value="45">45 min</option>
+                        <option value="60">1 hour</option>
+                        <option value="120">2 hour</option>
+                        <option value="150">2 hours 30 min</option>
+                        <option value="180">3 hours</option>
+                        <option value="210">3 hours 30 min</option>
+                        <option value="240">4 hours</option>
+                    </select>
+                    <label></label>
                 </div>
-
-                 <div className="input-field">
-                    <input type="text" name="recurring" ref="recurring" />
-                    <label htmlFor="recurring">Recurring</label>
-                </div>
+                <div className="input-field col s12">
+            
+                    <select value={this.state.recurring} name="recurring" onChange={this.handleInputChange.bind(this)}>
+                        <option value="" disabled selected>Recurring</option>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                        
+                    </select>
+                    <label></label>
+              </div>
                 <input type="submit" value="Save" className="btn" />
             
               </form>
